ade5b4ff82b4df8e78b1b2cac837f3d4e50edeef
Shivam Agarwal <itisshivam22@gmail.com>
2021-11-05 19:53:57 +0530

    Replace text with dice icons and make them work

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+    static defaultProps = {
+        numberWords: ["one", "two", "three", "four", "five", "six"]
+    };
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
@@ -10,15 +13,10 @@ class Die extends Component {
         this.props.handleClick(this.props.idx);
     }
     render() {
-        return (
-            <button
-                className={"Die"}
-                style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-                onClick={this.handleClick}
-            >
-                {this.props.val}
-            </button>
-        );
+        const { numberWords, locked, val, disabled } = this.props;
+        let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+        if (locked) classes += "Die-locked";
+        return <i className={classes} onClick={this.handleClick} disabled={disabled} />;
     }
 }
 
